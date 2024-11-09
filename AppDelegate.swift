b1fcbca2928fe19fbecd39461faf1d542ea581a6
Vadim <vadim.gusyatnikov@gmail.com>
2024-11-09 14:46:42 +0200

    Update AppDelegate.swift

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -8,7 +8,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     // Set API URL here or fetch it from a config.
     let apiURL = URL(string: "https://api.waqi.info/feed/limassol/?token=123")!
-    let iconURL = URL(string: "https://raw.githubusercontent.com/vadgus/debug/refs/heads/main/air.png")!
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         // Create the status bar item
@@ -18,7 +17,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             statusButton.title = "." // Initial placeholder text
             statusButton.action = #selector(updateOnClick) // Add click action
             statusButton.target = self
-            downloadAndSetIcon() // Download and set icon
             fetchDataAndUpdateStatusBar() // Initial data fetch on launch
             startPolling() // Set up periodic updates
         }
@@ -27,23 +25,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let menu = NSMenu()
         menu.addItem(NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q"))
         statusItem?.menu = menu
-    }
-
-    func downloadAndSetIcon() {
-        URLSession.shared.dataTask(with: iconURL) { data, response, error in
-            guard let data = data, error == nil, let icon = NSImage(data: data) else {
-                print("Failed to download icon:", error ?? "Unknown error")
-                return
-            }
-            
-            DispatchQueue.main.async { [weak self] in
-                if let statusButton = self?.statusItem?.button {
-                    statusButton.image = icon
-                    statusButton.image?.size = NSSize(width: 18, height: 18) // Resize as needed
-                    statusButton.image?.isTemplate = true // Adapts icon to system theme
-                }
-            }
-        }.resume()
     }
 
     @objc func updateOnClick() {
